6da2022fd8899bfc6adf70396029798934b01a81
ValentinaSimonova20 <simonova.valentina19@gmail.com>
2020-09-19 01:44:47 +0300

    Adding entities - Book, Author and Publisher. Adding Repositories. Interacting with entities by CommandLineRunner

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,57 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design","123123");
+        Publisher petr = new Publisher("Petr","Pushkina","Moscow","Russia","zip");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+        petr.getBooks().add(ddd);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+        publisherRepository.save(petr);
+
+        Author rod = new Author("Rob", "Johnson");
+        Book noEJD = new Book("J2EE Development without EJB", "534254325");
+        Publisher pushkin = new Publisher("Alex","Pushkina","Moscow","Russia","zip");
+        rod.getBooks().add(noEJD);
+        noEJD.getAuthors().add(rod);
+        pushkin.getBooks().add(noEJD);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJD);
+        publisherRepository.save(pushkin);
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of Books: "+bookRepository.count());
+        System.out.println("Number of Publisher Books: "+publisherRepository.count());
+
+    }
+}
---
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -0,0 +1,99 @@
+package guru.springframework.spring5webapp.domain;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+                inverseJoinColumns = @JoinColumn(name = "author_id") ) // таблица автор-книга
+    private Set<Author> authors = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(name = "publisher_book", joinColumns = @JoinColumn(name = "book_id"),
+                inverseJoinColumns = @JoinColumn(name = "publisher_id"))  // таблица издатель - книга
+    private Set<Publisher> publishers = new HashSet<>();
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn) {
+        this.title = title;
+        this.isbn = isbn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthor(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", author=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id != null ? id.equals(book.id) : book.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    public Set<Publisher> getPublishers() {
+        return publishers;
+    }
+
+    public void setPublishers(Set<Publisher> publishers) {
+        this.publishers = publishers;
+    }
+}
